53faf9311d6c6f1590bf2c76625d9595f3d7b28f
Andrew LIU <andrewliuzihua@gmail.com>
2020-08-10 11:47:43 +1000

    Update test.py

--- test.py
+++ test.py
@@ -27,6 +27,6 @@ def lambda_handler(event, context):
     direct_output = subprocess.check_output("ogr2ogr", shell=True)
     print(direct_output)
     
-    cmd = f"/opt/bin/ogr2ogr -f CSV -dialect sqlite -sql 'SELECT ST_X(ST_Centroid(ST_Transform(geometry, 4326))) AS Long, ST_Y(ST_Centroid(ST_Transform(geometry, 4326))) AS Lat, * FROM outputt' outputt.csv AU_Buildings.json"
-    os.system(cmd)
+    #cmd = f"/opt/bin/ogr2ogr -f CSV -dialect sqlite -sql 'SELECT ST_X(ST_Centroid(ST_Transform(geometry, 4326))) AS Long, ST_Y(ST_Centroid(ST_Transform(geometry, 4326))) AS Lat, * FROM outputt' outputt.csv AU_Buildings.json"
+    #os.system(cmd)
     
