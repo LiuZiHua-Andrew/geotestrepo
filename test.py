dbc4302b28e8528fbbe3344a9c1e441363b7becf
Andrew LIU <andrewliuzihua@gmail.com>
2020-08-10 11:57:19 +1000

    Update test.py

--- test.py
+++ test.py
@@ -5,8 +5,10 @@ import json
 import subprocess
 
 #s3_client = boto3.client('s3')
-try:
-    direct_output = subprocess.run(["ls", "-l"])
+
+try:  
+    cmd = f"/opt/bin/ogr2ogr -f CSV -dialect sqlite -sql 'SELECT ST_X(ST_Centroid(ST_Transform(geometry, 4326))) AS Long, ST_Y(ST_Centroid(ST_Transform(geometry, 4326))) AS Lat, * FROM outputt' outputt.csv AU_Buildings.json"
+    os.system(cmd)
 except subprocess.CalledProcessError as e:        
     print(e.output)
 
