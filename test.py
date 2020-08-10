89b399f30757db2cc30344263ab880bc740c3062
Andrew LIU <andrewliuzihua@gmail.com>
2020-08-10 11:35:36 +1000

    Create test.py

---
+++ test.py
@@ -0,0 +1,28 @@
+import boto3
+import os
+import sys
+import uuid
+import json
+
+s3_client = boto3.client('s3')
+
+def lambda_handler(event, context):
+    bucketname="lambda-test-andrew"
+    inputfilename="AU_Buildings.json"
+    outputfilePath="/tmp/output.json"
+    convertedfilePath="/tmp/converted.csv"
+    uploadfilename="converted.geojson"
+    
+    # s3_client.download_file(bucketname,inputfilename,outputfilePath)
+    
+    # ogr2ogr.main(["","-f","geojson","-t_srs","EPSG:4326",convertedfilePath,outputfilePath])
+    
+    # f = open(convertedfilePath)
+    # print(json.load(f))
+    
+    # response = s3_client.upload_file(convertedfilePath, bucketname,uploadfilename)
+    # print(response)
+    #cmd="cd /opt/lib && ls"
+    cmd = f"/opt/bin/ogr2ogr -f CSV -dialect sqlite -sql 'SELECT ST_X(ST_Centroid(ST_Transform(geometry, 4326))) AS Long, ST_Y(ST_Centroid(ST_Transform(geometry, 4326))) AS Lat, * FROM outputt' outputt.csv AU_Buildings.json"
+    os.system(cmd)
+    
